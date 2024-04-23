0a07e14a78f0478797b562251cec7d6ed685ea35
Austin Patton <austin.patton@arcadiascience.com>
2024-04-23 16:25:46 -0700

    addessed review and broke into subfunctions

--- code/raman-prediction-functions.R
+++ code/raman-prediction-functions.R
@@ -7,20 +7,27 @@ require(tidyverse)
 require(ggplot2)
 require(dplyr)
 
-# A function to do all remaining analyses using these prepared data
-predict_from_raman <-
-  function(spectral_data, response_var, n_bs_reps = 5000) {
-    # Summarize across replicates within pepper to get the
-    # median pixel (wave number)
-    # for each pepper
-    suppressMessages(suppressWarnings(
-      median_intensity <-
-        spectral_data |>
-        group_by(id, Pixel) |> # nolint
-        summarize(Pixel = mean(Pixel),
-                  MedianIntensity = median(Intensity)) |> # nolint
-        ungroup()
-    ))
+# A function to get the median profile of the Raman spectra
+get_median_profile <-
+  function(spectral_data) {
+    ## Function to get the median profile of the Raman spectra
+    ## Parameters:
+    ## spectral_data: data.frame containing three key columns:
+    ## - "id" (sample ID - not replicate)
+    ## - "Pixel" (i.e. wave number)
+    ## - "Intensity" (spectral intensity)
+    ## Returns:
+    ## A matrix containing the median profile of the Raman spectra
+
+    # Summarize across replicates to get the median pixel
+    # (wave number) for each sample
+    median_intensity <- spectral_data |>
+    group_by(id, Pixel) |> # nolint
+    summarize(
+      Pixel = mean(Pixel),
+      MedianIntensity = median(Intensity), # nolint
+      .groups = "drop"
+    )
 
     # Now prepare for analysis with Lasso regression
     median_profile <-
@@ -28,15 +35,19 @@ predict_from_raman <-
     rownames(median_profile) <- median_profile$id
     median_profile <- median_profile[, -1]
 
-    # Assuming flesh_pcs is your dataframe
-    predictors <- as.matrix(median_profile)
+    # Return a matrix for compatibility with glmnet
+    return(list(median_intensity = median_intensity,
+                predictors = as.matrix(median_profile)))
+  }
 
-    message("Conducting Lasso (L1) regression...")
+# A function to run the Lasso regression with bootstrapping
+run_lasso_bootstrap <-
+  function(predictors, response_var, n_bs_reps, sample_size, nfolds = 10) {
+    # Conduct bootstrap replicates in parallel with a progress bar (pblapply)
     lasso_res <-
       pblapply(1:n_bs_reps, function(x) {
-        sample_size <- floor(0.75 * nrow(predictors))
-
-        # Initialize a flag to manage error-handling
+        # Initialize a flag to manage error-handling (in the case of imbalanced
+        # training/test sets)
         success_flag <- FALSE
 
         # Loop until the code runs without error
@@ -53,8 +64,8 @@ predict_from_raman <-
             cv_lasso <- NULL
             suppressWarnings(
               cv_lasso <-
-                cv.glmnet(x = train_predictors, y = train_response, alpha = 1,
-                          grouped = FALSE, nfolds = 10)
+                cv.glmnet(x = train_predictors, y = train_response,
+                          grouped = FALSE, nfolds = nfolds)
             )
             # If we don't hit an error error, exit the loop
             if (!is.null(cv_lasso)) {
@@ -65,7 +76,9 @@ predict_from_raman <-
             print(paste("Error occurred:", e$message))
           })
         }
-        # Now summarize
+        # Now summarize, obtaining the best lambda from the cross validation,
+        # optimal coefficients, and predictions, quantifying the model's
+        # accuracy with mean squared error (MSE) and variance explained
         best_lambda <- cv_lasso$lambda.min  # Best lambda
         lasso <-
           glmnet(x = train_predictors, y = train_response, alpha = 1,
@@ -82,14 +95,19 @@ predict_from_raman <-
           mse <- NA
           variance_explained <- NA
         }
-
+        # And return outputs
         return(list(optimal_coefficients = optimal_coefficients,
                     mse = mse,
                     cv_lasso = cv_lasso,
                     variance_explained = variance_explained))
       }, cl = 9L)
 
-    message("Summarizing results...")
+    return(lasso_res)
+  }
+
+# A function to summarize the results of the Lasso regression
+summarize_lasso_bootstrap <-
+  function(lasso_res, n_bs_reps) {
     # Pull out the coefficients
     bs_coefs <-
       do.call(cbind, lapply(lasso_res, function(x) x$optimal_coefficients[, 1]))
@@ -127,9 +145,21 @@ predict_from_raman <-
                                          bs_coefs_summary$UpperCI < 0)] <- "Yes"
     bs_coefs_summary$times_selected <- times_selected
 
-    message("Plotting...")
+    bs_mse <- unlist(lapply(lasso_res, function(x) x$mse))
+    bs_mse <- data.frame(bs_rep = 1:n_bs_reps, mse = bs_mse)
+
+    return(list(bs_coefs_summary = bs_coefs_summary,
+                bs_mse = bs_mse))
+  }
+
+# A function to plot the results of the Lasso regression
+plot_lasso_results <-
+  function(lasso_res, lasso_summary, n_bs_reps) {
+    # Produce a "ramanhattan" plot of the mean coefficients at each point along
+    # the spectra, and the frequency with which they are retained across
+    # bootstrap replicates
     ramanhattan_plt <-
-      ggplot(bs_coefs_summary, aes(x = Pixel, color = Significant)) + # nolint
+      ggplot(lasso_summary$bs_coefs_summary, aes(x = Pixel, color = Significant)) + # nolint
       geom_hline(yintercept = 0, size = 0.5, alpha = 0.5, lty = 2) +
       geom_point(alpha = 0.75, aes(y = Mean, size = times_selected/n_bs_reps)) + # nolint
       scale_size(range = c(0.05, 5)) +
@@ -141,23 +171,24 @@ predict_from_raman <-
       theme_classic(base_size = 14) +
       theme(legend.position = "bottom")
 
-    bs_mse <- unlist(lapply(lasso_res, function(x) x$mse))
-    bs_mse <- data.frame(bs_rep = 1:n_bs_reps, mse = bs_mse)
+    # A histogram of the mean squared error (MSE) of predictions
+    # for each bootstrap replicate
     suppressWarnings(
       mse_plt <-
-        ggplot(bs_mse, aes(mse)) + # nolint
+        ggplot(lasso_summary$bs_mse, aes(mse)) + # nolint
         geom_histogram(fill = "grey", color = "black", bins = 50) +
-        geom_vline(xintercept = mean(na.omit(bs_mse$mse)),
+        geom_vline(xintercept = mean(na.omit(lasso_summary$bs_mse$mse)),
                    alpha = 0.75, size = 0.75, lty = 2, color = "red") +
         theme_classic(base_size = 14) +
         theme(legend.position = "bottom") +
         ylab("Frequency in BS Replicates") +
         xlab("Mean Squared Error (MSE)")
     )
+    # Calculate % variance explained for each bootstrap replicate and plot
+    # using histogram of the mean squared error (MSE) of predictions
     var_exp <-
       unlist(lapply(lasso_res, function(x) x$variance_explained))
     var_exp <- data.frame(bs_rep = 1:n_bs_reps, variance_explained = var_exp)
-
     suppressWarnings(
       var_exp_plt <-
         ggplot(var_exp, aes(variance_explained)) + # nolint
@@ -171,20 +202,79 @@ predict_from_raman <-
         ylab("Frequency in BS Replicates") +
         xlab("% Variance Explained")
     )
+    # Combine the plots into a single grid
     suppressWarnings(
       final_ramanhattan_plt <-
         plot_grid(ramanhattan_plt,
                   plot_grid(mse_plt, var_exp_plt, ncol = 1, align = "v"),
                   ncol = 2, rel_widths = c(3, 1))
     )
-    message("Done!")
-    return(list(
-      median_sample_intensity = median_intensity,
-      lasso_results = lasso_res,
-      bs_coefficient_summary = bs_coefs_summary,
-      ramanhattan_plot = ramanhattan_plt,
-      var_explained_plot = var_exp_plt,
-      mse_plot = mse_plt,
-      combined_final_plot = final_ramanhattan_plt
-    ))
+    return(list(ramanhattan_plt = ramanhattan_plt,
+                mse_plot = mse_plt,
+                var_explained_plot = var_exp_plt,
+                combined_final_plot = final_ramanhattan_plt))
+  }
+
+# A function to format the Raman spectra and predict the response
+# variable using a Lasso regression with bootstrapping
+predict_from_raman <-
+  function(spectral_data, response_var,
+           n_bs_reps = 5000, training_prop = 0.75,
+           plot_results = TRUE) {
+    ## Function to predict some quantitative response variable from Raman
+    ## spectra using lasso regression and a bootstrapping procedure to
+    ## estimate the stability and CI of the coefficients.
+    ## The function also plots the results.
+
+    ## Parameters:
+    ## spectral_data: data.frame containing three key columns:
+    ## - "id" (sample ID - not replicate)
+    ## - "Pixel" (i.e. wave number)
+    ## - "Intensity" (spectral intensity)
+    ## n_bs_reps: # of lasso regression bootstrap replicates
+    ## response_var: vector of response variables to predict, ordered by "id"
+
+    # Convert the spectra to a matrix of median profiles for each sample
+    median_profile <- get_median_profile(spectral_data)
+
+    # use a 75/25 split for training/testing
+    sample_size <- floor(training_prop * nrow(median_profile$predictors))
+
+    message("Conducting Lasso (L1) regression bootstrap procedure...")
+    lasso_res <-
+      run_lasso_bootstrap(median_profile$predictors, response_var,
+                          n_bs_reps, sample_size)
+
+    message("Summarizing results...")
+    lasso_summary <- summarize_lasso_bootstrap(lasso_res, n_bs_reps)
+
+    # If not plotting the results, return the results as a list,
+    # otherwise plot and proceed and include the plots in the output
+    if (plot_results == FALSE) {
+      message("Done!")
+      return(list(
+        median_sample_intensity = median_profile$median_intensity,
+        median_profile = median_profile$predictors,
+        lasso_results = lasso_res,
+        bs_coefficient_summary = lasso_summary
+      ))
+    } else {
+      message("Plotting...")
+      lasso_plots <-
+        plot_lasso_results(lasso_res, lasso_summary, n_bs_reps)
+
+      message("Done!")
+      # And return the results, converting the median profile to a data.frame
+      # with an "id" column
+      return(list(
+        median_sample_intensity = median_profile$median_intensity,
+        median_profile = median_profile$predictors,
+        lasso_results = lasso_res,
+        bs_coefficient_summary = lasso_summary,
+        ramanhattan_plot = lasso_plots$ramanhattan_plt,
+        var_explained_plot = lasso_plots$var_explained_plot,
+        mse_plot = lasso_plots$mse_plot,
+        combined_final_plot = lasso_plots$combined_final_plot
+      ))
+    }
   }
